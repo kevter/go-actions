cb234606a1fae639bb672ef47253400e66493b1b
Kevin <kevinzuniga2791@gmail.com>
2021-07-16 15:20:07 -0600

    changes

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"io"
+	"fmt"
+	"html"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-
-	http.HandleFunc("/", ExampleHandler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -17,12 +19,9 @@ func main() {
 	}
 
 	log.Println("** Service Started on Port " + port + " **")
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
-}
+	// if err := http.ListenAndServe(":"+port, nil); err != nil {
+	// 	log.Fatal(err)
+	// }
 
-func ExampleHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "application/json")
-	io.WriteString(w, `{"status":"ok"}`)
+	http.ListenAndServe(":"+port, nil)
 }
